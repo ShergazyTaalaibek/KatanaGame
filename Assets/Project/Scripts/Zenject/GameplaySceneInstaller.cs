3ccdef82969642466783d41952c43bf1f59ea159
Sher <143084222+ShergazyTaalaibek@users.noreply.github.com>
2023-10-13 22:06:18 +0600

    New
    Zenject
    UniRX
    Folders reorganizing
    Shaders

---
+++ Assets/Project/Scripts/Zenject/GameplaySceneInstaller.cs
@@ -0,0 +1,9 @@
+using Zenject;
+
+public class GameplaySceneInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        
+    }
+}
---
+++ Assets/Project/Scripts/Zenject/GlobalInstaller.cs
@@ -0,0 +1,9 @@
+using Zenject;
+
+public class GlobalInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        
+    }
+}
